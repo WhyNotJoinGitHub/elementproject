a8428b623d66d61632e53eaa4b1e05af2b3da66d
WhyNotJoinGitHub <1206816624@qq.com>
2020-05-13 20:04:53 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
   <div id="app">
-
+    <el-button>999</el-button>
   </div>
 </template>
 
-<style lang="scss">
+<script>
+  export default {
+    name: 'app',
+    components: {},
+    data() {
+      return {
+        radio: "1",
+        value1: new Date(2016, 9, 10, 18, 40),
+        value2: new Date(2016, 9, 10, 18, 40)
+      }
+    },
+    methods:{
+      radioChange(e){
+        console.log(e);
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
 
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import {Button} from 'element-ui';
+import './plugins/element.js'
+
+Vue.use(Button)
 
 Vue.config.productionTip = false
 
