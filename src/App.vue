4dea503809ee1d8932ea6b5c4a258c2f15122726
WhyNotJoinGitHub <1206816624@qq.com>
2020-05-13 20:06:13 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -10,15 +10,9 @@
     components: {},
     data() {
       return {
-        radio: "1",
-        value1: new Date(2016, 9, 10, 18, 40),
-        value2: new Date(2016, 9, 10, 18, 40)
       }
     },
     methods:{
-      radioChange(e){
-        console.log(e);
-      }
     }
   }
 </script>
