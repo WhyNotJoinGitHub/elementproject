c04b5e864944fa42ae2c38de8e8349cb724b6751
WhyNotJoinGitHub <1206816624@qq.com>
2020-05-15 18:36:54 +0800

    登录页

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,4 @@
 </script>
 
 <style lang="scss">
-  * {
-    padding: 0;
-    margin: 0;
-    box-sizing: border-box;
-  }
-  html,body,#app{
-    width: 100%;
-    height: 100%;
-  }
 </style>
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,19 +1,89 @@
 <template>
   <div class="login">
-    登录页
+    <div class="login-container">
+      <el-form :model="loginForm" :rules="loginFormRules" ref="loginFormRef">
+        <el-form-item prop="username">
+          <el-input v-model="loginForm.username"
+                    prefix-icon="el-icon-user-solid"
+                    placeholder="请输入用户名"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input v-model="loginForm.password"
+                    prefix-icon="el-icon-lock"
+                    type="password"
+                    placeholder="请输入密码"
+          ></el-input>
+        </el-form-item>
+        <el-form-item class="loginBtn">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-    name: "Login"
+    name: "Login",
+    data() {
+      return {
+        loginForm: {
+          username: 'admin',
+          password: '123456'
+        },
+        loginFormRules: {
+          username: [
+            {required: true, message: '请输入用户名', trigger: 'blur'},
+          ],
+          password: [
+            {required: true, message: '请输入密码', trigger: 'blur'},
+          ]
+        }
+      }
+    },
+    methods: {
+      resetLoginForm() {
+        this.$refs.loginFormRef.resetFields();
+      },
+      login() {
+        this.$refs.loginFormRef.validate(async (valid) => {
+          if (!valid) return;
+          const {data: result} = await this.$http.post("login", this.loginForm)
+          if (result.meta.status!==200) return console.log("登录失败")
+          console.log("登录成功")
+        })
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
-  .login{
+  .login {
     background: url("~assets/img/login/loginBanner.png") no-repeat top;
     width: 100%;
     height: 100%;
+    position: relative;
+  }
+
+  .login-container {
+    width: 360px;
+    height: 300px;
+    margin: auto;
+    padding: 20px;
+    background: #fff;
+    border-radius: 10px;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+
+  .loginBtn {
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 </style>
