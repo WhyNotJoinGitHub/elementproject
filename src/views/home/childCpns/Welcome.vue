55bf774492c97b38f422eeacfa42d2da7e432c9a
WhyNotJoinGitHub <1206816624@qq.com>
2020-06-03 17:36:35 +0800

    3

--- src/views/home/childCpns/Welcome.vue
+++ src/views/home/childCpns/Welcome.vue
@@ -1,15 +1,49 @@
 <template>
   <div class="welcome">
-    welcome
+    <ul class="box">
+      <li
+        v-for="(item,index ) in cities"
+        :class="{checked:arr.includes(index)}"
+        @click="checkedBox(index)"
+        :key="index">
+        <button>{{item}}</button>
+        </li>
+    </ul>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "Welcome"
+export default {
+  name: "Welcome",
+  data() {
+    return {
+      cities: ["上海", "北京", "广州", "重庆", "西安"],
+      arr: []
+    };
+  },
+  methods: {
+    checkedBox(i) {
+      if (this.arr.includes(i)) {
+        //includes()方法判断是否包含某一元素,返回true或false表示是否包含元素，对NaN一样有效
+        //filter()方法用于把Array的某些元素过滤掉，filter()把传入的函数依次作用于每个元素，然后根据返回值是true还是false决定保留还是丢弃该元素：生成新的数组
+        this.arr = this.arr.filter(function(ele) {
+          return ele != i;
+        });
+      } else {
+        this.arr.push(i);
+      }
+    }
   }
+};
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+li {
+  list-style: none;
+  display: inline-block;
+}
+.checked button{
+  background: lightBlue;
+  color: #fff;
+}
 </style>
--- src/views/home/childCpns/goodsManage/Params.vue
+++ src/views/home/childCpns/goodsManage/Params.vue
@@ -1,133 +1,150 @@
 <template>
-	<div class="CategoryParams">
-		<el-breadcrumb separator-class="el-icon-arrow-right">
-			<el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
-			<el-breadcrumb-item>商品管理</el-breadcrumb-item>
-			<el-breadcrumb-item>参数列表</el-breadcrumb-item>
-		</el-breadcrumb>
-		<el-card>
-			<el-alert title="注意：只允许为第三级分类设置相关参数！" type="warning" show-icon :closable="false"></el-alert>
-			<el-row>
-				<el-col>
-					<span>选择商品分类：</span>
-					<el-cascader v-model="selectData" 
-								:options="shopCategory" 
-								:props="shopCategoryProps" 
-								@change="handleChange" 
-								clearable></el-cascader>
-				</el-col>
-			</el-row>
-			<el-tabs v-model="activeName" @tab-click="handleClick">
-				<el-tab-pane label="动态参数" name="many">
-					<el-button type="primary" size="mini" :disabled="isDisabled">添加参数</el-button>
-					<el-table :data="manyTableData" border style="width: 100%">
-						<template slot="empty">
-							<img src="~assets/img/no-data.png" alt="" />
-						</template>
-						<el-table-column type="expand" width="80">
-							<template v-slot="scop">
-								{{ scop.row }}
-							</template>
-						</el-table-column>
-						<el-table-column type="index" label="序号" width="80"></el-table-column>
-						<el-table-column prop="attr_name" label="参数名称"></el-table-column>
-						<el-table-column label="操作">
-							<el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
-							<el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
-						</el-table-column>
-					</el-table>
-				</el-tab-pane>
-				<el-tab-pane label="静态属性" name="only">
-					<el-button type="primary" size="mini" :disabled="isDisabled">添加属性</el-button>
-					<el-table :data="onlyTableData" border style="width: 100%">
-						<template slot="empty">
-							<img src="~assets/img/no-data.png" alt="" />
-						</template>
-						<el-table-column type="expand" width="80">
-							<template v-slot="scop">
-								{{ scop.row }}
-							</template>
-						</el-table-column>
-						<el-table-column type="index" label="序号" width="80"></el-table-column>
-						<el-table-column prop="attr_name" label="属性名称"></el-table-column>
-						<el-table-column label="操作">
-							<el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
-							<el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
-						</el-table-column>
-					</el-table>
-				</el-tab-pane>
-			</el-tabs>
-		</el-card>
-	</div>
+  <div class="CategoryParams">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>参数列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <el-alert title="注意：只允许为第三级分类设置相关参数！" type="warning" show-icon :closable="false"></el-alert>
+      <el-row>
+        <el-col>
+          <span>选择商品分类：</span>
+          <el-cascader
+            v-model="selectData"
+            :options="shopCategory"
+            :props="shopCategoryProps"
+            @change="handleChange"
+            clearable
+          ></el-cascader>
+        </el-col>
+      </el-row>
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="动态参数" name="many">
+          <el-button type="primary" size="mini" :disabled="isDisabled">添加参数</el-button>
+          <el-table :data="manyTableData" border style="width: 100%">
+            <template slot="empty">
+              <img src="~assets/img/no-data.png" alt />
+            </template>
+            <el-table-column type="expand" width="80">
+              <template v-slot="scop">
+                <el-tag v-for="(tag,index) in scop.row.attr_vals" :key="index" closable>{{tag}}</el-tag>
+              </template>
+            </el-table-column>
+            <el-table-column type="index" label="序号" width="80"></el-table-column>
+            <el-table-column prop="attr_name" label="参数名称"></el-table-column>
+            <el-table-column label="操作">
+              <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+              <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+        <el-tab-pane label="静态属性" name="only">
+          <el-button type="primary" size="mini" :disabled="isDisabled">添加属性</el-button>
+          <el-table :data="onlyTableData" border style="width: 100%">
+            <template slot="empty">
+              <img src="~assets/img/no-data.png" alt />
+            </template>
+            <el-table-column type="expand" width="80">
+              <template v-slot="scop">
+                <el-tag v-for="(tag,index) in scop.row.attr_vals" :key="index" closable>{{tag}}</el-tag>
+              </template>
+            </el-table-column>
+            <el-table-column type="index" label="序号" width="80"></el-table-column>
+            <el-table-column prop="attr_name" label="属性名称"></el-table-column>
+            <el-table-column label="操作">
+              <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+              <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+            </el-table-column>
+          </el-table>
+        </el-tab-pane>
+      </el-tabs>
+    </el-card>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: 'Params',
-		data() {
-		return {
-			shopCategory: [],
-			selectData: [],
-			shopCategoryProps: {
-				value: 'cat_id',
-				label: 'cat_name',
-				children: 'children',
-				expandTrigger: 'hover'
-			},
-			activeName: 'many',
-			manyTableData: [],
-			onlyTableData: []
-		};
-	},
-	created() {
-		this.getShopCategory();
-	},
-	methods: {
-		//获取分类数据
-		async getShopCategory() {
-			const { data: res } = await this.$http.get('categories');
-			if (res.meta.status !== 200) return this.$message.error('获取分类数据失败！');
-			this.shopCategory = res.data;
-		},
+export default {
+  name: "Params",
+  data() {
+    return {
+      shopCategory: [],
+      selectData: [],
+      shopCategoryProps: {
+        value: "cat_id",
+        label: "cat_name",
+        children: "children",
+        expandTrigger: "hover"
+      },
+      activeName: "many",
+      manyTableData: [],
+      onlyTableData: []
+    };
+  },
+  created() {
+    this.getShopCategory();
+  },
+  methods: {
+    //获取分类数据
+    async getShopCategory() {
+      const { data: res } = await this.$http.get("categories");
+      if (res.meta.status !== 200)
+        return this.$message.error("获取分类数据失败！");
+      this.shopCategory = res.data;
+    },
 
-		//下拉框改变获取值
-		handleChange() {
-			this.getParamsList();
-		},
+    //下拉框改变获取值
+    handleChange() {
+      this.getParamsList();
+    },
 
-		//tab切换
-		handleClick() {
-			this.getParamsList();
-		},
-		//获取参数列表
-		async getParamsList() {
-			if (this.selectData.length !== 3) return (this.selectData = []);
-			const { data: res } = await this.$http.get(`categories/${this.cateId}/attributes`, { params: { sel: this.activeName } });
-			if (res.meta.status !== 200) return this.$message.error('获取分类参数失败！');
-			if (this.activeName == 'many') {
-				this.manyTableData = res.data;
-			} else {
-				this.onlyTableData = res.data;
-			}
-		}
-	},
-	computed: {
-		 isDisabled() {
-			return this.selectData.length !== 3;
-		},
-		//获取分类 ID
-		cateId() {
-			if (this.selectData.length == 3) {
-				return this.selectData[2];
-			}
-			return null;
-		}
-	}
+    //tab切换
+    handleClick() {
+      this.getParamsList();
+    },
+    //获取参数列表
+    async getParamsList() {
+      if (this.selectData.length !== 3) return (this.selectData = []);
+      const { data: res } = await this.$http.get(
+        `categories/${this.cateId}/attributes`,
+        {
+          params: { sel: this.activeName }
+        }
+      );
+      if (res.meta.status !== 200)
+        return this.$message.error("获取分类参数失败！");
+
+      res.data.forEach(item => {
+        item.attr_vals = item.attr_vals ? item.attr_vals.split(" ") : [];
+      });
+      console.log(res.data);
+      if (this.activeName == "many") {
+        this.manyTableData = res.data;
+      } else {
+        this.onlyTableData = res.data;
+      }
+    }
+  },
+  computed: {
+    isDisabled() {
+      return this.selectData.length !== 3;
+    },
+    //获取分类 ID
+    cateId() {
+      if (this.selectData.length == 3) {
+        return this.selectData[2];
+      }
+      return null;
+    }
+  }
 };
 </script>
 
 <style scoped>
 .el-row {
-	margin: 15px 0;
+  margin: 15px 0;
+}
+.el-tag {
+  margin: 5px;
 }
 </style>
